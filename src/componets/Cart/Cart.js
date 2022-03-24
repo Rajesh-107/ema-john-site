373df0467accd130d393d62e686d6ede3755dbd5
Rajesh-107 <applerb326@gmail.com>
2022-03-25 01:36:41 +0600

    quantity added

--- src/componets/Cart/Cart.js
+++ src/componets/Cart/Cart.js
@@ -6,8 +6,10 @@ const Cart = (props) => {
     
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
     }
     const tax = parseFloat((total * 0.1).toFixed(2));
@@ -17,7 +19,7 @@ const Cart = (props) => {
     return (
         <div className='cart'>
               <h2>Order Summary</h2>
-                <p>Selected Item: {cart.length}</p>
+                <p>Selected Item: {quantity}</p>
                 <p>Total Price: ${total} </p>
                 <p>Total Shipping: ${shipping} </p>
                 <p>Tax: {tax}</p>
