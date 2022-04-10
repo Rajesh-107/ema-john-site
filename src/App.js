f3749c6141f2febc601ab1dd7afc1653f8f4bbad
Rajesh-107 <applerb326@gmail.com>
2022-04-10 19:41:17 +0600

    handle signin and signup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import About from './componets/About/About';
 import Header from './componets/Header/Header';
 import Inventory from './componets/Inventory/Inventory';
+import Login from './componets/Login/Login';
 import Orders from './componets/Orders/Orders';
 import Shop from './componets/Shop/Shop';
+import SignUp from './componets/SignUp/SignUp';
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
         <Route path='/orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -2,5 +2,6 @@
 1. create new project in console.google.com
 2. npm install firebase
 3. create firebase.init.js and import  auth.
-4. go to authentication and enabale 
+4. go to authentication and enable gmail provider.
+5. 
 */ 
